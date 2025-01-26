b7e20ac773e5272e73edd8c7dcbbaa17f268c34a
30189028 <surkhab.mundi@ucalgary.ca>
2025-01-25 20:56:10 -0700

    Finished project. Not including api .env file

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const Transaction = require('./models/Transaction.js');
+const {connect} = require('mongoose');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get('/api/test', (req, res) => {
+    res.json('test ok333');
+});
+
+app.post('/api/transaction', async (req, res) => {
+    await connect(process.env.MONGO_URL);
+    const {name,description,datetime,price} = req.body;
+    const transaction = await Transaction.create({name,description,datetime,price});
+    res.json(transaction);
+});
+
+app.get('/api/transactions' , async (req, res) => {
+    await connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find();
+    res.json(transactions);
+
+});
+
+app.listen(4040);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
 
 function App() {
+    const [name, setName] = useState('');
+    const [datetime, setDatetime] = useState('');
+    const [description, setDescription] = useState('');
+    const [transactions, setTransactions] = useState([]);
+
+    useEffect(() => {
+        getTransactions().then(setTransactions);
+    }, []);
+
+    async function getTransactions() {
+        const url = process.env.REACT_APP_API_URL+'/transactions';
+        const response = await fetch(url);
+        return await response.json();
+    }
+
+
+    function addNewTransaction(ev) {
+        ev.preventDefault();
+        const url = process.env.REACT_APP_API_URL+'/transaction';
+        const price = name.split(" ")[0];
+        fetch(url, {
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify({
+                price,
+                name:name.substring(price.length+1),
+                description,
+                datetime,
+            })
+        }).then(response => {
+            response.json().then(json => {
+                setName('');
+                setDatetime('');
+                setDescription('');
+                console.log('result', json);
+            });
+        });
+    }
+
+    let balance = 0;
+    for (const transaction of transactions) {
+        balance += transaction.price;
+    }
+
+    balance = balance.toFixed(2);
+    const fraction = balance.split('.')[1];
+    balance = balance.split('.')[0];
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <main>
+          <h1>${balance}<span>{fraction}</span></h1>
+          <form onSubmit={addNewTransaction}>
+              <div className="basic">
+                  <input type="text"
+                         value={name}
+                         onChange={ev => setName(ev.target.value)}
+                         placeholder="Price here. Add a +/- in front!"/>
+                  <input type="datetime-local"
+                         value={datetime}
+                         onChange={ev => setDatetime(ev.target.value)}/>
+              </div>
+              <div className="description">
+                  <input type="text"
+                         value={description}
+                         onChange={ev => setDescription(ev.target.value)}
+                         placeholder="Write a description here!"/>
+              </div>
+              <button type={"submit"}>Add a new transaction</button>
+          </form>
+          <div className="transactions">
+              {transactions.length > 0 && transactions.map(transaction => (
+                  <div className="transaction">
+                      <div className="left">
+                          <div className="name">{transaction.name}</div>
+                          <div className="description">{transaction.description}</div>
+                      </div>
+                      <div className="right">
+                          <div className={"price " +(transaction.price<0 ?'red':'green')}>{transaction.price}</div>
+                          <div className="datetime">{transaction.datetime}</div>
+                      </div>
+                  </div>
+              ))}
+          </div>
+      </main>
   );
 }
 
